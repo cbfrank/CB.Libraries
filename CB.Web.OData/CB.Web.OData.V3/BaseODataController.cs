9910acd64ec3f286a3329fe5960398fe093127e0
cb <cb@cb.com>
2014-12-31 15:30:17 +0800

    1.0

---
+++ CB.Web.OData/CB.Web.OData.V3/BaseODataController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Query;
+using CB.Data.Common.CRUD;
+using CB.Ioc;
+
+namespace CB.Web.OData
+{
+    public abstract class BaseODataController<T, TKey> : ODataController
+        where T : class, IIdKeyEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        protected BaseODataController()
+        {
+            DisposeService = true;
+        }
+
+        protected bool DisposeService { get; private set; }
+
+        [Dependency]
+        public ICRUDService<T, TKey> Service { get; set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Service != null && DisposeService && (Service is IDisposable))
+                {
+                    ((IDisposable)Service).Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        protected virtual T PreProcess(T entity, CRUDAction action)
+        {
+            return entity;
+        }
+
+        public virtual IQueryable<T> Get()
+        {
+            return Service.Query();
+        }
+
+        public virtual SingleResult<T> Get([FromODataUri] TKey key)
+        {
+            var result = Service.Query().Where(p => p.Id.Equals(key));
+            return SingleResult.Create(result);
+        }
+
+        public virtual async Task<IHttpActionResult> Post(T entity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            entity = await Service.CreateAsync(PreProcess(entity, CRUDAction.Create));
+            return Created(entity);
+        }
+
+        public virtual async Task<IHttpActionResult> Put([FromODataUri] TKey key, T entity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            entity = await Service.UpdateAsync(PreProcess(entity, CRUDAction.Update));
+            return Updated(entity);
+        }
+
+        public virtual async Task<IHttpActionResult> Delete([FromODataUri] TKey key)
+        {
+            await Service.DeleteAsync(key);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        public virtual HttpResponseMessage GetCount(ODataQueryOptions<T> queryOptions)
+        {
+            var query = queryOptions.ApplyTo(Get());
+            var queryResults = query as IQueryable<object>;
+            var count = queryResults.Count();
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(count.ToString(CultureInfo.InvariantCulture), Encoding.UTF8, "text/plain")
+            };
+            return response;
+        }
+    }
+}
---
+++ CB.Web.OData/CB.Web.OData.V3/SupportForOData.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.OData.Routing;
+using System.Web.Http.OData.Routing.Conventions;
+using Microsoft.Data.Edm;
+using Microsoft.Data.Edm.Library;
+
+namespace CB.Web.OData
+{
+    public class CountODataRoutingConvention : EntitySetRoutingConvention
+    {
+        public override string SelectAction(ODataPath odataPath, HttpControllerContext controllerContext, ILookup<string, HttpActionDescriptor> actionMap)
+        {
+            if (controllerContext.Request.Method == HttpMethod.Get && odataPath.PathTemplate == "~/entityset/$count")
+            {
+                if (actionMap.Contains("GetCount"))
+                {
+                    return "GetCount";
+                }
+            }
+            return null;
+        }
+    }
+
+    public class CountODataPathHandler : DefaultODataPathHandler
+    {
+        protected override ODataPathSegment ParseAtEntityCollection(IEdmModel model, ODataPathSegment previous, IEdmType previousEdmType, string segment)
+        {
+            if (segment == "$count")
+            {
+                return new CountPathSegment();
+            }
+            return base.ParseAtEntityCollection(model, previous, previousEdmType, segment);
+        }
+    }
+
+    public class CountPathSegment : ODataPathSegment
+    {
+        public override string SegmentKind
+        {
+            get
+            {
+                return "$count";
+            }
+        }
+
+        public override IEdmType GetEdmType(IEdmType previousEdmType)
+        {
+            return EdmCoreModel.Instance.FindDeclaredType("Edm.Int32");
+        }
+
+        public override IEdmEntitySet GetEntitySet(IEdmEntitySet previousEntitySet)
+        {
+            return previousEntitySet;
+        }
+
+        public override string ToString()
+        {
+            return "$count";
+        }
+    }
+}
